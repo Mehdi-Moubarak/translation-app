9c410e18eb563ef589d9910fdc9cbe574bd786ee
Mehdi-Moubarak <mehdi.moubarak01@gmail.com>
2023-12-13 17:07:40 +0100

    added selection

--- script.js
+++ script.js
@@ -1,73 +1,76 @@
-const words = ['hello', 'name', 'accept', 'car', 'day']
-const translatedWords = ['salut', 'nom', 'accepter', 'voiture', 'jour']
+const words = ['hello', 'name', 'accept', 'car', 'day'];
+const translatedWords = ['salut', 'nom', 'accepter', 'voiture', 'jour'];
+
 renderWords();
 renderTranslatedWords();
+
 function renderWords() {
     const innerContainer = document.querySelector('.innerContainer');
-    for (let i = words.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [words[i], words[j]] = [words[j], words[i]];
-    }
     const wordsList = document.createElement('div');
     wordsList.className = "column-1";
+    
     words.forEach((word) => {
-        const wordElement = document.createElement('div');
-        wordElement.className = "word";
-        wordElement.textContent = word;
-        wordElement.addEventListener('click', function (e) {
-            changeWordStyle('default', e.target);
-        })
+        const wordElement = createWordElement(word);
         wordsList.appendChild(wordElement);
     });
+
     innerContainer.appendChild(wordsList);
 }
+
 function renderTranslatedWords() {
-    for (let i = translatedWords.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [translatedWords[i], translatedWords[j]] = [translatedWords[j], translatedWords[i]];
-    }
     const innerContainer = document.querySelector('.innerContainer');
     const translatedWordsList = document.createElement('div');
     translatedWordsList.className = "column-2";
+    
     translatedWords.forEach((word) => {
-        const wordElement = document.createElement('div');
-        wordElement.className = "word";
-        wordElement.textContent = word;
-        wordElement.addEventListener('click', function (e) {
-            changeWordStyle('default', e.target);
-        })
+        const wordElement = createWordElement(word);
         translatedWordsList.appendChild(wordElement);
     });
+
     innerContainer.appendChild(translatedWordsList);
 }
-function changeWordStyle(Case = null, word) {
-    var selected;
-    const words = document.querySelectorAll('.column-1 .word')
-    for (let i = 0; i < words.length; i++) {
-        if (words[i].classList.contains('wordStyle') && !words[i].classList.contains('already_selected')) {
-            console.log(words[i].classList.contains('already_selected'))
-            selected = true;
-            break;
+
+function createWordElement(word) {
+    const wordElement = document.createElement('div');
+    wordElement.className = "word";
+    wordElement.textContent = word;
+    wordElement.addEventListener('click', handleWordClick);
+    return wordElement;
+}
+
+function handleWordClick(e) {
+    const clickedWord = e.target;
+    const column = clickedWord.closest('.innerContainer').querySelector(`.${clickedWord.classList.contains('column-1') ? 'column-1' : 'column-2'}`);
+
+    // Deselect all words in the same column
+    column.querySelectorAll('.word').forEach(word => {
+        if (word !== clickedWord) {
+            word.classList.remove('wordStyle');
         }
-    }
+    });
 
-    console.log(selected)
-    if (Case == 'default' && !selected) {
-        word.classList.toggle('wordStyle');
-        if (!word.classList.contains('already_selected')) {
+    // Toggle the selected style for the clicked word
+    clickedWord.classList.toggle('wordStyle');
 
-            word.classList.add('already_selected');
-        } else {
-            word.classList.remove('already_selected');
+    // Check if one word is selected from each column
+    const column1SelectedWord = document.querySelector('.column-1 .wordStyle');
+    const column2SelectedWord = document.querySelector('.column-2 .wordStyle');
+
+    if (column1SelectedWord && column2SelectedWord) {
+        // Check if the selected words match based on their index
+        const index1 = Array.from(column.querySelector('.column-1').children).indexOf(column1SelectedWord);
+        const index2 = Array.from(column.querySelector('.column-2').children).indexOf(column2SelectedWord);
+
+        if (index1 === index2) {
+            // Matched, update the style
+            column1SelectedWord.classList.add('wordStyle-2');
+            column2SelectedWord.classList.add('wordStyle-2');
         }
-
-    } else if (Case == 'match') {
-        word.classList.toggle('wordStyle-2');
-    } else {
-        // word.classList.toggle('wordStyle-2');
     }
 }
+
+
+
 function GameManager() {
-
+    addClickEventListeners();
 }
-GameManager();
